d403a921ee21930654e6eed274ba0a56488717d7
Sean DeNigris <code@clipperadams.com>
2024-05-23 22:37:28 -0400

    [CI]: Workflow including Artifact Release & Helper Scripts

---
+++ scripts/package_gtoolkit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+project_name=$1
+smalltalk_name=$2
+build_folder=$3
+echo "Packaging release of ${project_name} on ${smalltalk_name} from ${build_folder}"
+mkdir release
+pushd release > /dev/null
+cp "${build_folder}/TravisCI.image" "${project_name}.image"
+cp "${build_folder}/TravisCI.changes" "${project_name}.changes"
+cp "${build_folder}/*.sources" ./
+cp -R "${build_folder}/pharo-local" ./
+#Next two lines are only differences from Pharo
+cp -R "${build_folder}/gt-extra" ./
+cp -R "${build_folder}/vm" ./
+
+#Not sure if this is relevant in the GT world, since it's for Launcher. Maybe just for degubbing?
+#echo "${smalltalk_name}" | sed -e 's/.*\-//g ; s/\..*//g ; s/$/0/' > pharo.version
+
+zip -r ../$project_name.zip ./* > /dev/null
+popd > /dev/null
