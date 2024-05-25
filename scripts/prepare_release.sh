28e57f15da8626b14bcedc2b63c66e454b7293c1
Sean DeNigris <code@clipperadams.com>
2024-05-25 00:03:16 -0400

    [CI]: Reasonable Version 1 - Allow SSH Key via ssh-agent   - Pass CI SSH Key Secret   - Only Add SSH Key if Present - Guard GT & Pharo Specific Commands - Prevent errors in output - Release GT VM & (On Linux) Libs - Only Mac ARM - Runner Matrix & GH Token Only for stCI - Reusable workflow inputs - required and default are incompatible

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env sh
-build_name=$1
-smalltalk_name=$2
-build_folder=$3
+
+readonly build_name="$1"
+readonly smalltalk_name="$2"
+readonly build_folder="$3"
+
+source "$build_folder/../helpers.sh"
+source "$build_folder/../gtoolkit/run.sh"
+
+is_gt() {
+    [[ "$smalltalk_name" == GToolkit* ]]
+}
+
+is_pharo() {
+    [[ "$smalltalk_name" == Pharo* ]]
+}
+
+gt_vm_folder() {
+    local result
+    
+    if is_mac_build; then
+        result="GlamorousToolkit.app"
+    else
+        result="bin"
+    fi
+    
+    echo "${result}"
+}
+
 echo "Packaging release of ${build_name} on ${smalltalk_name} from ${build_folder}"
 mkdir release
 pushd release > /dev/null
@@ -9,12 +34,18 @@ cp "${build_folder}/TravisCI.image" "${build_name}.image"
 cp "${build_folder}/TravisCI.changes" "${build_name}.changes"
 cp "${build_folder}"/*.sources ./
 cp -R "${build_folder}/pharo-local" ./
-#Next two lines are only differences from Pharo
-cp -R "${build_folder}/gt-extra" ./
-cp -R "${build_folder}/vm" ./
 
-#Not sure if this is relevant in the GT world, since it's for Launcher. Maybe just for degubbing?
-#echo "${smalltalk_name}" | sed -e 's/.*\-//g ; s/\..*//g ; s/$/0/' > pharo.version
+if is_gt; then
+    cp -R "${build_folder}/gt-extra" ./
+    cp -R "${build_folder}/$(gt_vm_folder)" ./
+    if is_linux_build; then
+        cp -R "${build_folder}/lib" ./
+    fi
+fi
+
+if is_pharo; then
+    echo "${smalltalk_name}" | sed -e 's/.*\-//g ; s/\..*//g ; s/$/0/' > pharo.version
+fi
 
 zip -r ../$build_name.zip ./* > /dev/null
 popd > /dev/null
